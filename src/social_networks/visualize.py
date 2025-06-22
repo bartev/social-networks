4d6c2c8086a06c71bd3ad764b7757cc2e2702923
Bartev Vartanian <bartev@gmail.com>
2025-06-22 01:01:13 -0500

    Add Week 3 quiz (with supporting code)

--- src/social_networks/visualize.py
+++ src/social_networks/visualize.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import mplcursors
 import networkx as nx
 from matplotlib import cm as cm
+from networkx import Graph, MultiDiGraph
 
 
-def draw_pretty_hover(G, scale_by_degree=False, color_by_degree=False):
+def draw_pretty_hover(G: Graph, scale_by_degree=False, color_by_degree=False):
     """Draw a pretty graph with hover.
 
     Parameters:
@@ -61,3 +62,36 @@ def draw_pretty_hover(G, scale_by_degree=False, color_by_degree=False):
     plt.tight_layout()
     plt.show()
     return fig
+
+
+def draw_big_multi_directed(G: MultiDiGraph, figsize=(4, 3), seed=1):
+    """Draw `G` with large nodes, curved arrows, and multi-edges"""
+    plt.figure(figsize=figsize)
+    plt.clf()
+    plt.cla()
+    pos = nx.spring_layout(G, seed=seed)
+
+    nx.draw_networkx_nodes(G, pos=pos, node_size=1500, node_color="skyblue")
+    nx.draw_networkx_labels(
+        G, pos, font_size=16, font_color="white", font_weight="bold"
+    )
+    nx.draw_networkx_edges(
+        G,
+        pos=pos,
+        arrowstyle="-|>",
+        arrowsize=20,
+        edge_color="black",
+        connectionstyle="arc3, rad=0.2",
+        min_source_margin=15,
+        min_target_margin=20,  # to end arrows away from the center of the node
+    )
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()
+
+
+def is_dark(rgb_color):
+    """Convert RGB to perceived brightness (0=dark, 1=bright)"""
+    r, g, b = rgb_color[:3]
+    brightness = 0.299 * r + 0.587 * g + 0.114 * b
+    return brightness < 0.5
